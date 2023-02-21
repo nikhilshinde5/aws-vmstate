37cc4bbaccc263202227cc15050d115f13d536b8
Ubuntu <ubuntu@ip-172-31-93-74.ec2.internal>
2023-02-21 05:09:39 +0000

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM registry.access.redhat.com/ubi8/go-toolset:1.18.4-8.1669838000
+FROM registry.access.redhat.com/ubi8/ubi-minimal
 
 # Labels
 LABEL name="aws-vmstate" \
-    maintainer="xyzcompany.com" \
-    vendor="xyzcompany" \
+    maintainer="nikhil.com" \
+    vendor="nikhil" \
     version="1.0.0" \
     release="1" \
     summary="This service enables state management of AWS cloud vms." \
@@ -17,14 +17,17 @@ RUN chgrp -R 0 /opt && \
     chmod +x -R /opt
 
 USER 1001
+ENV ec2_tag_key "Name"
+ENV ec2_tag_value "nikhil-aws-ec2"
 
-COPY go.* ./
-COPY aws-vmstate.go .
+# COPY go.* ./
+COPY aws-vmstate .
+# ADD data data/
 
-RUN go mod download
 
-RUN go build -o aws-vmstate
+# RUN go mod download
+# RUN go build -o aws-vmstate
 
 #RUN chmod +x /opt/aws-vmstate
 
-CMD ["bash","-c","/opt/aws-vmstate "]
+CMD ["bash","-c","/opt/aws-vmstate -n $ec2_tag_key -v $ec2_tag_value"]
--- aws-vmstate.go
+++ aws-vmstate.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"flag"
+	"os"
 	"time"
 
 	//"flag"
@@ -26,6 +29,17 @@ type EC2StartInstancesAPI interface {
 	StartInstances(ctx context.Context,
 		params *ec2.StartInstancesInput,
 		optFns ...func(*ec2.Options)) (*ec2.StartInstancesOutput, error)
+	ModifyInstanceAttribute(ctx context.Context,
+		params *ec2.ModifyInstanceAttributeInput,
+		optFns ...func(*ec2.Options)) (*ec2.ModifyInstanceAttributeOutput, error)
+	StopInstances(ctx context.Context,
+		params *ec2.StopInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.StopInstancesOutput, error)
+}
+
+type ConfigMap struct {
+	InstanceType string `json:"instance_type"`
+	ImageId      string `json:"image_id"`
 }
 
 // StartInstance starts an Amazon Elastic Compute Cloud (Amazon EC2) instance.
@@ -39,6 +53,15 @@ type EC2StartInstancesAPI interface {
 //
 //	If success, a StartInstancesOutput object containing the result of the service call and nil.
 //	Otherwise, nil and an error from the call to StartInstances.
+
+func UpdateInstanceAttribute(c context.Context, api EC2StartInstancesAPI, input *ec2.ModifyInstanceAttributeInput) (*ec2.ModifyInstanceAttributeOutput, error) {
+	return api.ModifyInstanceAttribute(c, input)
+}
+
+func PauseInstances(c context.Context, api EC2StartInstancesAPI, input *ec2.StopInstancesInput) (*ec2.StopInstancesOutput, error) {
+	return api.StopInstances(c, input)
+}
+
 func StartInstances(c context.Context, api EC2StartInstancesAPI, input *ec2.StartInstancesInput) (*ec2.StartInstancesOutput, error) {
 	resp, err := api.StartInstances(c, input)
 
@@ -76,16 +99,39 @@ func init() {
 
 }
 func main() {
+	name := flag.String("n", "", "The name of the tag to attach to the instance")
+	value := flag.String("v", "", "The value of the tag to attach to the instance")
 
+	flag.Parse()
+
+	if *name == "" || *value == "" {
+		fmt.Println("You must supply a name and value for the tag (-n NAME -v VALUE)")
+		return
+	}
+
+	file, err := os.Open("data/config.json")
+	if err != nil {
+		fmt.Println("Error opening config file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var config ConfigMap
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		fmt.Println("Error decoding config:", err)
+		os.Exit(1)
+	}
+
+	var instanceType = config.InstanceType
 	for {
 
 		var instanceIds = make([]string, 0)
 		input1 := &ec2.DescribeInstancesInput{
 			Filters: []types.Filter{
 				{
-					Name: aws.String("tag:POC"),
+					Name: aws.String("tag:" + *name),
 					Values: []string{
-						"GolangOperator",
+						*value,
 					},
 				},
 			},
@@ -108,6 +154,64 @@ func main() {
 				}
 
 				fmt.Println(instanceIds)
+			}
+
+			file, err := os.Open("data/config.json")
+			if err != nil {
+				fmt.Println("Error opening config file:", err)
+				os.Exit(1)
+			}
+			defer file.Close()
+
+			var config ConfigMap
+			if err := json.NewDecoder(file).Decode(&config); err != nil {
+				fmt.Println("Error decoding config:", err)
+				os.Exit(1)
+			}
+
+			var newInstanceType = config.InstanceType
+			if newInstanceType != instanceType {
+
+				fmt.Printf("Found change in instance type: %v --> %v \n", instanceType, newInstanceType)
+
+				instanceID := instanceIds[0]
+
+				//time.Sleep(30 * time.Second)
+
+				//Stopping instances before changing instance type
+
+				fmt.Println("Stopping instances before changing instance type")
+				stopInstancesInput := &ec2.StopInstancesInput{
+					InstanceIds: []string{instanceID},
+					Force:       aws.Bool(false),
+				}
+				_, err = PauseInstances(context.TODO(), client, stopInstancesInput)
+				if err != nil {
+					fmt.Println("Got an error stoping the instance:")
+					fmt.Println(err)
+					return
+				}
+
+				//this sleep for letting ec2 stop
+				time.Sleep(60 * time.Second)
+
+				fmt.Println("Updating instance type of instance with ID: ", instanceID)
+				attributeInput := &ec2.ModifyInstanceAttributeInput{
+					InstanceId: &instanceID,
+					InstanceType: &types.AttributeValue{
+						Value: &newInstanceType,
+					},
+				}
+
+				_, err = UpdateInstanceAttribute(context.TODO(), client, attributeInput)
+				if err != nil {
+					fmt.Println("Got an error updating the instance:")
+					fmt.Println(err)
+					return
+				}
+
+				instanceType = newInstanceType
+				fmt.Println("Modified instance type of ec2 instance with Instance ID: ", instanceID)
 			}
 
 			input := &ec2.DescribeInstanceStatusInput{
